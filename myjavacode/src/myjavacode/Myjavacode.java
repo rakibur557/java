b077322d97d8263d89894fd74f3153913dd595f3
Rakibur Rahman <62113329+rakibur557@users.noreply.github.com>
2021-11-05 22:19:37 +0600

    Update Myjavacode.java

--- myjavacode/src/myjavacode/Myjavacode.java
+++ myjavacode/src/myjavacode/Myjavacode.java
@@ -1,21 +1,15 @@
-//package myjavacode;
-//
-//public class Myjavacode {
-//
-//    public static void main(String[] args) {
-//        System.out.println("Rakibur Rahman");
-//        System.out.println("01736121557");
-//        System.out.println("Bsc in CSE");
-//    }
-//    
-//}
 package myjavacode;
 public class Myjavacode{
     public static void main(String[] args) {
         System.out.println("Hello Java");
+        //To use NewLine use println or \n
+        System.out.print("Rakibur Rahman\n");
+        System.out.println("01736121557");
+        // TO Print value of a variable use ( +variable_name )
+        int num =10;
+        System.out.println("num = "+num);
     }
 }
         
-
 
 
