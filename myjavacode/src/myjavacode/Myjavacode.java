0730cbeed19eaffe1bbc64472d410717d91d8c40
Rakibur Rahman <62113329+rakibur557@users.noreply.github.com>
2021-11-05 22:15:00 +0600

    update

--- myjavacode/src/myjavacode/Myjavacode.java
+++ myjavacode/src/myjavacode/Myjavacode.java
@@ -1,11 +1,21 @@
+//package myjavacode;
+//
+//public class Myjavacode {
+//
+//    public static void main(String[] args) {
+//        System.out.println("Rakibur Rahman");
+//        System.out.println("01736121557");
+//        System.out.println("Bsc in CSE");
+//    }
+//    
+//}
 package myjavacode;
-
-public class Myjavacode {
-
+public class Myjavacode{
     public static void main(String[] args) {
-        System.out.println("Rakibur Rahman");
-        System.out.println("01736121557");
-        System.out.println("Bsc in CSE");
+        System.out.println("Hello Java");
     }
-    
 }
+        
+
+
+
