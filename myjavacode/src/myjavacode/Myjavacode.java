4fb86155a7acfe5dfd5749bb96693bd421a08a90
Rakibur Rahman <62113329+rakibur557@users.noreply.github.com>
2021-11-05 21:45:04 +0600

    Add files via upload
    first commit

---
+++ myjavacode/src/myjavacode/Myjavacode.java
@@ -0,0 +1,11 @@
+package myjavacode;
+
+public class Myjavacode {
+
+    public static void main(String[] args) {
+        System.out.println("Rakibur Rahman");
+        System.out.println("01736121557");
+        System.out.println("Bsc in CSE");
+    }
+    
+}
